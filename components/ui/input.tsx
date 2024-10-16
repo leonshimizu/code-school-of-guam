b2f7c11e27e31209ae73806cf4a1d94ae860094f
Leon Shimizu <lmshimizu@gmail.com>
2024-10-17 04:50:17 +1000

    Remove the interface

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,13 +1,7 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
+import * as React from "react";
+import { cn } from "@/lib/utils";
 
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
-
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
@@ -19,9 +13,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
